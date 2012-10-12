a8772b8f06fed436cd1d078b567f5daebfe36b4b
Vipul Nayyar <nayyar23@gmail.com>
2012-10-13 02:17:33 +0530

    cdn

--- work.js
+++ work.js
@@ -41,36 +41,3 @@ function at(){
 
 }
 
-
-function uploadSuccess(result) {
-  alert("Upload was successful");
-  console.log("Bytes sent: " + result.bytesSent);
-  console.log("Response code: " + result.responseCode);
-  console.log("Response: " + result.response);
-}
-
-function uploadError(result) {
-  alert("Upload failed");
-  console.log("Error code: " + result.code);
-  console.log("Source: " + result.source);
-  console.log("Target: " + result.target);
-  console.log("HTTP Status: " + result.https_status);
-}
-
-function fileUpload() {
-  var parameters, options;
-  try {
-    parameters = { app : "webworks" };
-    options = {
-      fileKey : "file",
-      fileName : "blackberry.jpg",
-      mimeType : "image/jpeg",
-      params : parameters,
-      chunkedMode : true,
-      chunkSize : 1024
-    };
-    blackberry.io.filetransfer.upload("/accounts/1000/shared/camera/image_123.jpg", "http://www.blackberry.com/upload", uploadSuccess, uploadError, options);
-  } catch(e) {
-    alert("Exception in fileUpload: " + e);
-  }
-}
