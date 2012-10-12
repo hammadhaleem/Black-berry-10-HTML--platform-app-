55990e0df86c009066971b763c3861cef48d193d
hammadhaleem <hammadhaleem@gmail.com>
2012-10-12 21:50:15 +0530

    g

---
+++ work.js
@@ -0,0 +1,36 @@
+    function ready() {
+         var ele = document.createElement("div");
+         ele.innerHTML = "uuid: " + blackberry.identity.uuid;
+         document.documentElement.appendChild(ele);
+      }
+      window.addEventListener("load", function(e) {
+         document.addEventListener("webworksready", ready);
+      }, false);
+
+      var img = document.createElement('img');
+img.ontouchstart=function(e) { 
+
+for(var i = 0; I < e. touches.length; i++)
+{
+   var touch = touches[i];
+
+   var x = touch.clientX;
+   var y = touch.clientY;
+
+   // do something
+}
+
+function takePic() {
+  try {
+    blackberry.media.camera.takePic(successCB);
+  } catch(e) {
+    alert("Error in supported: " + e);
+  }
+}
+
+function successCB(filePath) {
+  blackberry.media.camera.close();
+  // do something with filePath
+}
+
+
